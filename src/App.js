0aa56c36bd4589de15e8bd7cad8b04d5a7bab74a
Andres <cesarandrestorresbernal604@gmail.com>
2024-08-10 17:21:36 -0500

    primer modulo finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Heading from "./Heading"
+import Card from "./Card";
+function Header() {
+  return <h1>Hola mundo</h1>;
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+              <Header/>
+              <Heading firstName="Empleo" />
+              <Heading firstName="Andrés"/>
+              <h1>Task: Add three Card elements</h1>
+              <Card h2="First card's h2" h3="First card's h3" />
+              <Card h2="Second card's h2" h3="Second card's h3" />
+              <Card h2="Third card's h2" h3="Third card's h3" />
+          </div>
   );
-}
+};
 
 export default App;
